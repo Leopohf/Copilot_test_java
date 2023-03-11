4819d5db9c2235a75591c004242c8a26fbe9c1b3
Jorge Leonardo Hernandez Fajardo <leopohf@gmail.com>
2023-03-11 17:01:11 -0500

    adding math functions created by copilot

---
+++ src/com/example/Math/Function/MathFunctions.java
@@ -0,0 +1,54 @@
+package com.example.Math.Function;
+
+public class MathFunctions {
+
+    // Returns the addition of two numbers
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    // Returns the subtraction of two numbers
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // Returns the multiplication of two numbers
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Returns the division of two numbers
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+    // Returns the remainder of two numbers
+    public static int remainder(int a, int b) {
+        return a % b;
+    }
+
+    // Returns the remainder of two numbers
+    public static int max(int a, int b) {
+        return Math.max(a, b);
+    }
+
+    // Returns the power of a number
+    public static int pow(int a, int b) {
+        return (int) Math.pow(a, b);
+    }
+
+    // Returns the square root of a number
+    public static int sqrt(int a) {
+        return (int) Math.sqrt(a);
+    }
+
+    // Returns the absolute value of a number
+    public static int abs(int a) {
+        return Math.abs(a);
+    }
+
+    // Returns the hypotenuse of a right triangle
+    public static int hypotenuse(int a, int b) {
+        return (int) Math.hypot(a, b);
+    }
+}
--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,7 +1,42 @@
 package com.example.helloworld;
 
+import com.example.Math.Function.MathFunctions;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("Testing Java Copilot");
+
+        // Test MathFunctions
+        System.out.println("Testing MathFunctions");
+
+        // Test add
+        System.out.println("add(1, 2) = " + MathFunctions.add(1, 2));
+
+        // Test subtract
+        System.out.println("subtract(1, 2) = " + MathFunctions.subtract(1, 2));
+
+        // Test multiply
+        System.out.println("multiply(1, 2) = " + MathFunctions.multiply(1, 2));
+
+        // Test divide
+        System.out.println("divide(1, 2) = " + MathFunctions.divide(1, 2));
+
+        // Test remainder
+        System.out.println("remainder(1, 2) = " + MathFunctions.remainder(1, 2));
+
+        // Test max
+        System.out.println("max(1, 2) = " + MathFunctions.max(1, 2));
+
+        // Test pow
+        System.out.println("pow(1, 2) = " + MathFunctions.pow(1, 2));
+
+        // Test sqrt
+        System.out.println("sqrt(1) = " + MathFunctions.sqrt(1));
+
+        // Test abs
+        System.out.println("abs(1) = " + MathFunctions.abs(1));
+
+        // Test hypotenuse
+        System.out.println("hypotenuse(1, 2) = " + MathFunctions.hypotenuse(1, 2));
     }
 }
